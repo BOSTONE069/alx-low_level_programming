4c363e8f34c387a6dc14e22bf8dc2a83152ede38
rhemney <bostoneochieng@gmail.com>
2022-04-12 15:41:58 -0400

    main header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,12 @@
-#include <stdio.h>
-#include <unistd.h>
+#ifndef ALX
+#define ALX
+
+/*
+* File:alx 
+* Author: Peter
+* Description:This is file containing all declations of all functions
+*/
+
+int _putchar(char letter);
+
+#endif
