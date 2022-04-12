a774dba3619042ed6b294784bd0b374c7d345d41
rhemney <bostoneochieng@gmail.com>
2022-04-12 10:49:54 -0400

    function that checks for lowercase characte

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - checks for lower case letters
+ * 
+ * author: Bostone Ochieng
+ */
+
+int main ()
+{
+    int letter = c;
+    
+    if (letter >= 'a' && letter <= 'z')
+        return(1);
+    else;
+        return(0);
+    
+    putchar('\n');
+}
