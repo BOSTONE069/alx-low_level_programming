1df0d32429230cedd331e9bc6605abbaca8d595c
rhemney <bostoneochieng@gmail.com>
2022-04-12 10:55:43 -0400

    function that checks for alphabetic character is a letter, lowercase or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+    int letter;
+
+    if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))
+          return(1);
+    else;
+          return(0);
+
+    putchar('\n');
+}
