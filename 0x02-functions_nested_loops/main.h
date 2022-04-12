2e3951db177bf8200b55d2cf67eb737c69d42f79
rhemney <bostoneochieng@gmail.com>
2022-04-12 15:57:11 -0400

    main header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,18 @@
 * Description:This is file containing all declations of all functions
 */
 
-int _putchar(int letter);
+int _putchar(char letter);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
