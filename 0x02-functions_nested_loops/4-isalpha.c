14b675ec6f836d77c31839676739bddb278e9402
rhemney <bostoneochieng@gmail.com>
2022-04-12 22:17:38 -0400

    checks alphabet characters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,6 +3,8 @@
 /**
  * _isalpha - This is checking whether a letter is upper or lowercase
  * Description: function for checking if a letter is a upper or lower case
+ * @c:This is the ineteger integer which is entered
+ * Return: Always 0 (Success)
  */
 int _isalpha(int c)
 {
