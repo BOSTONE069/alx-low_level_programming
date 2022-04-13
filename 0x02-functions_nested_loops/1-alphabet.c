d8504204d17b27bc7b47870eebaa4580fb979ac1
rhemney <bostoneochieng@gmail.com>
2022-04-12 21:30:10 -0400

    alphabets in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,12 @@
  */
 void print_alphabet(void)
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++){
+  char letter;
+
+  for (letter = 'a'; letter <= 'z'; letter++)
+  {
     _putchar(letter);
-}
-_putchar('\n');
+  }
+  _putchar('\n');
 
 }
