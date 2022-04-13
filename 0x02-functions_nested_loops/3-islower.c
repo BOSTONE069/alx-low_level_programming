810677ed942292f0561c3832db26ba3954fd7441
rhemney <bostoneochieng@gmail.com>
2022-04-12 22:36:00 -0400

    checks for lower charaters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,10 +3,11 @@
 /**
  * _islower - main block
  * Description: function that checks for lowercase letters
+ * @c:This is the inserted integer
+ * Return: Always 0 (Success)
  */
 int _islower(int c)
 {
-    
     if (c >= 'a' && c <= 'z'){
         return(1);
     }
