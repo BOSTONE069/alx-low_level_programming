93734eaa0ef0d34bdb7889a0e9af0b1be38cca68
rhemney <bostoneochieng@gmail.com>
2022-04-14 09:04:06 -0400

    This is for printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - main function for printing numbers
+ * @num: number to print
+ * Return: void
+ */
+void print_numbers(void)
+{
+        int num;
+
+        for (num = 0; num <=9 ; num++)
+        {
+                _putchar(num);
+        }
+        _putchar('\n');
+}
