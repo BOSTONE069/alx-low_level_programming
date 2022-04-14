429e0b0f3c13763bf92b6e83f3f424303774a975
rhemney <bostoneochieng@gmail.com>
2022-04-14 08:26:19 -0400

    This chacks for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - main block
+ * Description: function that checks for lowercase letters
+ * @c:This is the inserted integer
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return(1);
+    }
+    else
+    {
+        return(0);
+    }
+}
