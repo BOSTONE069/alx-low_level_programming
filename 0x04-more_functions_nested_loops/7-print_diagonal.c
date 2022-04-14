ead2eaeea8430780251d81007adc859721ad49aa
rhemney <bostoneochieng@gmail.com>
2022-04-14 14:59:26 -0400

    prints diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_diagonal - entry point
+ *
+ * Description: Prints diagonals
+ *
+ *@n: number of spaces
+ *@lines: number of lines to be printed
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int row, lines;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= n; row++)
+		{
+			if (row > 1)
+			{
+				for (lines = 1; lines <= row - 1; lines++)
+				{
+				_putchar(' ');
+				}
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
