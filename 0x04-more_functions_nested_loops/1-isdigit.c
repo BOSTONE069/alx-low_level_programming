f38265079791e389b9d23d7bcb2a5a6fabe26c5e
rhemney <bostoneochieng@gmail.com>
2022-04-14 13:08:49 -0400

    Program that help in checking for digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,11 +7,8 @@
  */
 int _isdigit(int c)
 {
-        for (c = 0; c <=9; c++)
-        {
                 if (c >= '0' && c <= '9')
                     return (1);
                 else
                     return (0);
-        }
 }
