5a574e924f5c87d8b3619e47c2ccd85ba1098d20
rhemney <bostoneochieng@gmail.com>
2022-04-13 20:45:43 -0400

    Main Header File

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef ALX
+#define ALX
+
+/*
+* File:alx
+* Author: Bostone
+* Description:This is file containing all declations of all functions
+*/
+
+void positive_or_negative(int i)
+
+#endif
