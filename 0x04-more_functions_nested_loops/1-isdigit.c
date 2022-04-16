a2b2f6ad967b935273cc34f1cc8eeeb0782464bf
rhemney <bostoneochieng@gmail.com>
2022-04-16 18:04:36 -0400

    function that checks for a digit (0 through 9).

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * _isdigit - function that checks for a digit (0 through 9).
- *@c: character or integer entered character
- * Return: Always
+ * _isdigit - input
+ *
+ * Description: define if a character is a number
+ * @c: charater
+ * Return: 1 yes 0 no
  */
+
 int _isdigit(int c)
 {
-                if (c >= '0' && c <= '9')
-                    return (1);
-                else
-                    return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
