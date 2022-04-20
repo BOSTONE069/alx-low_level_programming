b26e8df63d08b4fd7c635932e0b49a61f27d7ff9
rhemney <bostoneochieng@gmail.com>
2022-04-20 19:08:52 -0400

    This is function for copying a file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strcpy - This is main function for copying the files
+ *@dest: this is where to copy the string
+ *@src: source where the string is copied from.
+ *Return: pointer to the destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
