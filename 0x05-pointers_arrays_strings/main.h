9c13eece9a3151e26dbc9d70d029fc06f9888ad7
rhemney <bostoneochieng@gmail.com>
2022-04-20 17:27:49 -0400

    Main Header File

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,9 @@
  *Description: This file is the main header file and it contains all the decrations of all the functions in the program
  * */
 
-int _putchar(char num);
+#include <stdio.h>
+
+int _putchar(char c);
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
