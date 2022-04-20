97c9b4918572ba5f64f0499e54ddb3985da650c9
rhemney <bostoneochieng@gmail.com>
2022-04-20 18:08:16 -0400

    This is the main header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,10 +5,11 @@
  *File:alx
  *Author: Bostone
  *Description: This file is the main header file and it contains all the decrations of all the functions in the program
- * */
+ */
 
 #include <stdio.h>
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -19,5 +20,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
