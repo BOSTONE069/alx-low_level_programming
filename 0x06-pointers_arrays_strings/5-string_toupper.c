bfde8088639790309f59ddfdddee0ed20bd11802
rhemney <bostoneochieng@gmail.com>
2022-04-22 09:53:11 -0400

    This is for changing lower case to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *string_toupper - This is the main function for converting lower to upper case
+ *@str: This is the pointer string
+ *Return: void
+ */
+char *string_toupper(char *str)
+{
+_putchar(strupr(*str));
+}
