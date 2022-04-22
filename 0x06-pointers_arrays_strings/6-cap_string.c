e40aafc6e97c3559fdff9054165082c236a77cfc
rhemney <bostoneochieng@gmail.com>
2022-04-22 10:29:14 -0400

    This is for capitalizing all the first letters in the string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *cap_string - This is the main function
+ *@s: This is the string to be entered
+ *Return: void
+ */
+char *cap_string(char *s)
+{
+for (int i = 0; s[i] != '\0'; i++)
+{
+if (i == 0)
+{
+if ((s[i] >= 'a' && s[i] <= 'z'))
+s[i] = s[i] - 32;
+continue;
+}
+if (s[i] == ' ')
+{
+++i;
+
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+s[i] = s[i] - 32;
+continue;
+}
+}
+else
+{
+if (s[i] >= 'A' && s[i] <= 'z')
+s[i] = s[i] + 32;
+}
+}
+}
