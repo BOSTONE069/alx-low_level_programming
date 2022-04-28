4a981868421f352187c33fe30b8361aad7d64bb4
rhemney <bostoneochieng69@.com>
2022-04-28 11:24:56 -0400

    This recursion work

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,10 +9,15 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-	{
-		return (-1);
-	}
-	return (pow(x, y));
+	int result = x;
 
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (1);
+
+	result *= _pow_recursion(x, y - 1);
+
+	return (result);
 }
