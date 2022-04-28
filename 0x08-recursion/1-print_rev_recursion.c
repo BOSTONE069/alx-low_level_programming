91256a8d5151623960d9b11204201677163692c3
rhemney <bostoneochieng69@.com>
2022-04-28 11:12:27 -0400

    This recursion work

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,5 +7,15 @@
 */
 void _print_rev_recursion(char *s)
 {
-	strrev(s);
+	static int i, len, temp;
+	len = strlen(s);
+
+	if (i < len/2)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+		i++;
+		_print_rev_recursion(s);
+	}
 }
