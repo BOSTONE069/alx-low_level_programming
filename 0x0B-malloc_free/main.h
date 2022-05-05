32a4247c636e482867e529c95dbf617339d4e6a7
rhemney <bostoneochieng69@gmail.com>
2022-05-05 03:06:33 -0700

    Main header file

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,23 @@
+#ifndef ALX
+#define ALX
+
+/**
+ *File:alx
+ *Author: Bostone
+ *Description: This file is the main header file and it contains all the decrations of all the functions in the program
+ */
+
+#include <string.h>
+#include <stdio.h>
+
+int _putchar(char c);
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif
