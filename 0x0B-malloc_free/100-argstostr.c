9d33d186e53a0d564e63d0993e0bbae0ee0ec176
rhemney <bostoneochieng69@.com>
2022-05-05 20:38:52 -0400

    Malloc and Free Function

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,34 +12,34 @@
  */
 char *argstostr(int ac, char **av)
 {
-    char *str;
-    int arg, byte, index, size = ac;
+char *str;
+int arg, byte, index, size = ac;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-    for (arg = 0; arg < ac; arg++)
-    {
-        for (byte = 0; av[arg][byte]; byte++)
-            size++;
-    }
+for (arg = 0; arg < ac; arg++)
+{
+for (byte = 0; av[arg][byte]; byte++)
+size++;
+}
 
-    str = malloc(sizeof(char) * size + 1);
+str = malloc(sizeof(char) * size + 1);
 
-    if (str == NULL)
-        return (NULL);
+if (str == NULL)
+return (NULL);
 
-    index = 0;
+index = 0;
 
-    for (arg = 0; arg < ac; arg++)
-    {
-        for (byte = 0; av[arg][byte]; byte++)
-            str[index++] = av[arg][byte];
+for (arg = 0; arg < ac; arg++)
+{
+for (byte = 0; av[arg][byte]; byte++)
+str[index++] = av[arg][byte];
 
-        str[index++] = '\n';
-    }
+str[index++] = '\n';
+}
 
-    str[size] = '\0';
+str[size] = '\0';
 
-    return (str);
+return (str);
 }
