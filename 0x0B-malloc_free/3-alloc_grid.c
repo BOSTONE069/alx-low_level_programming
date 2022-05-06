c84181f1a91ccb8a68833ca9774e208392937f42
rhemney <bostoneochieng69@.com>
2022-05-05 20:34:06 -0400

    Malloc and Free Function

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,36 +12,36 @@
  */
 int **alloc_grid(int width, int height)
 {
-    int **twoD;
-    int hgt_index, wid_index;
+int **twoD;
+int hgt_index, wid_index;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
+if (width <= 0 || height <= 0)
+return (NULL);
 
-    twoD = malloc(sizeof(int *) * height);
+twoD = malloc(sizeof(int *) * height);
 
-    if (twoD == NULL)
-        return (NULL);
+if (twoD == NULL)
+return (NULL);
 
-    for (hgt_index = 0; hgt_index < height; hgt_index++)
-    {
-        twoD[hgt_index] = malloc(sizeof(int) * width);
+for (hgt_index = 0; hgt_index < height; hgt_index++)
+{
+twoD[hgt_index] = malloc(sizeof(int) * width);
 
-        if (twoD[hgt_index] == NULL)
-        {
-            for (; hgt_index >= 0; hgt_index--)
-                free(twoD[hgt_index]);
+if (twoD[hgt_index] == NULL)
+{
+for (; hgt_index >= 0; hgt_index--)
+free(twoD[hgt_index]);
 
-            free(twoD);
-            return (NULL);
-        }
-    }
+free(twoD);
+return (NULL);
+}
+}
 
-    for (hgt_index = 0; hgt_index < height; hgt_index++)
-    {
-        for (wid_index = 0; wid_index < width; wid_index++)
-            twoD[hgt_index][wid_index] = 0;
-    }
+for (hgt_index = 0; hgt_index < height; hgt_index++)
+{
+for (wid_index = 0; wid_index < width; wid_index++)
+twoD[hgt_index][wid_index] = 0;
+}
 
-    return (twoD);
+return (twoD);
 }
