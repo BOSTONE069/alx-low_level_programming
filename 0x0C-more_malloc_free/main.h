5cfadd17fdda61530f5ce017fc34ccf4055d1ece
rhemney <bostoneochieng69@gmail.com>
2022-05-10 01:46:28 -0700

    This is more malloc

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,20 @@
+#ifndef ALX
+#define ALX
+
+/**
+ *File:alx
+ *Author: Bostone
+ *Description: This file is the main header file and it contains all the decrations of all the functions in the program
+ */
+
+
+
+int _putchar(char c);
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
