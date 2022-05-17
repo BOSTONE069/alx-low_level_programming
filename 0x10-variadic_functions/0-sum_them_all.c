a22b9b201630baaef6f024846e1a4d3ded36fd44
rhemney <bostoneochieng69@gmail.com>
2022-05-17 07:14:07 -0700

    function for finding all the numbers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,24 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its paramters.
- * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
- *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
+ * sum_them_all - the function prototype
+ * @n: unsigned integer n declaration
+ * Return: total sum of parameters
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index, sum = 0;
+	int tot_sum = 0;
+	unsigned int  i;
+	va_list pntr;
 
-	va_start(nums, n);
+	if (n == 0)
+		return (0);
 
-	for (index = 0; index < n; index++)
-		sum += va_arg(nums, int);
+	va_start(pntr, n);
+	for (i = 0; i < n; i++)
+		tot_sum += va_arg(pntr, int);
+	va_end(pntr);
 
-	va_end(nums);
-
-	return (sum);
+	return (tot_sum);
 }
