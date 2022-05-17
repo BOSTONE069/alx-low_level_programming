d14f75445209184672ee191f8e222b90becd0bc1
rhemney <bostoneochieng69@gmail.com>
2022-05-16 21:56:39 -0700

    These are variadic functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef ALX
+#define ALX
+/**
+ *File:alx
+ *Author: Bostone
+ *Description: This file is the main header file and it contains all the decrations of all the functions in the program
+ */
+
+int _putchar(char c);
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
