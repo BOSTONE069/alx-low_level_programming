d9e0068c69266014ef0a2961d1cb6a606a428b26
rhemney <bostoneochieng69@gmail.com>
2022-05-24 03:21:55 -0700

    function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_list - function for printing all elements in a list.
+ * print_list - printing all elements in a list.
  * @h: The list_t list.
  *
  * Return: The number of nodes in h.
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
@@ -20,9 +20,9 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		count++;
+		nodes++;
 		h = h->next;
 	}
 
-	return (count);
+	return (nodes);
 }
