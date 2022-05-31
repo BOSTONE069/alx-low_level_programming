c9268e2cd46f12179a49a18fba8556f609d96e33
rhemney <bostoneochieng69@gmail.com>
2022-05-31 02:39:27 -0700

    Main Header File

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,11 +6,14 @@
  * AUth: OCHIENG BOSTONE
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x13-bit_manipulation directory.
- */
+*/
 
 int _putchar(char c);
+int _strlen(const char *s);
+int _pow_recursion(int x, int y);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+void recursion_print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
