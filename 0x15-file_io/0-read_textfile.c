202b26cb71a3ef8b7f895d0ba9c63633bda56e95
rhemney <bostoneochieng69@gmail.com>
2022-06-02 06:05:03 -0700

    function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,55 +1,39 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
 
 /**
- * read_textfile - function that reads a text file and
- * prints it to the POSIX standard output
- * @filename: name of the file to be read
- * @letters: the number of letters it should read and print
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * Return: actual number of letters it could read and print
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, read_count, write_count, close_status;
-	char *buf;
-	ssize_t actual_count;
+	ssize_t o, r, w;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
 
-	read_count = read(fd, buf, letters);
-	buf[letters] = '\0';
-	write_count = write(STDOUT_FILENO, buf, read_count);
-	if (write_count == -1)
-	{
-		free(buf);
-		return (0);
-	}
-	if (write_count != read_count)
-		return (0);
+	free(buffer);
+	close(o);
 
-	close_status = close(fd);
-	if (close_status == -1)
-		return (0);
-
-	free(buf);
-	actual_count = (ssize_t)write_count;
-	return (actual_count);
+	return (w);
 }
