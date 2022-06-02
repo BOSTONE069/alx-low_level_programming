5055567857e2c51bd4b67b26c789b40c1a7d2bb8
rhemney <bostoneochieng69@gmail.com>
2022-06-02 06:08:38 -0700

    This is the main header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,17 @@
 #ifndef FILE_H
 #define FILE_H
+/*
+ *Author: BOSTONE OCHIENG
+ *
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
