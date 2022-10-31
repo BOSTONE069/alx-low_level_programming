ca616b7610b35fefe90891cb9d1d6b0ed53b0d98
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-31 15:53:49 -0400

    introduction to c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
+	/* Printing the size of various types. */
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
